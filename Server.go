3bbaea9158366d56d12e6f1efd7036273450315e
Jeff Blue <517262600@qq.com>
2022-01-14 16:55:28 +0000

    Update Server.go

--- Server.go
+++ Server.go
@@ -121,10 +121,7 @@ func (s *ServerStatusManager) OpenPlayerSocket(w http.ResponseWriter, r *http.Re
 	}
 	// upgrade to websocket
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		if r.Header.Get("Origin")[:16] == "http://127.0.0.1" {
-			return true
-		}
-		return false
+		return true
 	}
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
